a2a0072f04a051e30da6ab668413e1f2b5e5751f
soony1995 <sanbonpeople@daum.net>
2022-06-17 20:02:53 +0900

    안드로이드 + 서버

--- mzroad/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from mzroad.models import BigData
-from mzroad.models import AI
-
-admin.site.register(BigData)
-admin.site.register(AI)
--- mzroad/bigData_serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from mzroad.models import BigData
-
-class Bigdata_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = BigData
-        fields = '__all__'
